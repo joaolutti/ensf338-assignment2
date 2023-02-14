7cd190615c46bc9e760d0e49f2d951dc85fef148
zepaulojr1 <64874053+zepaulojr1@users.noreply.github.com>
2023-02-14 20:50:21 -0300

    Update and rename plot.py to ex2.2.py

--- ex2.2.py
+++ ex2.2.py
@@ -1,15 +1,17 @@
 import json
 import timeit
+import matplotlib.pyplot as plt
 import sys
 sys.setrecursionlimit(20000)
+import threading
+threading.stack_size(33554432)
 
-
-    
 def func1(arr, low, high):
     if low < high:
         pi = func2(arr, low, high)
         func1(arr, low, pi-1)
         func1(arr, pi + 1, high)
+
 def func2(array, start, end):
     p = array[start]
     low = start + 1
@@ -26,11 +28,19 @@ def func2(array, start, end):
     array[start], array[high] = array[high], array[start]
     return high
 
-
 with open('ex2.json') as f:
-    inputs = json.load(f)
-i = 0
-for i, arr in enumerate(inputs['inputs']):
-    print(f"Input {i+1}: {len(arr)} elements")
+    data = json.load(f)
+
+times = []
+for i, arr in enumerate(data):
+    print(f"Input {i}: {len(arr)} elements")
     t = timeit.timeit(lambda: func1(arr, 0, len(arr)-1), number=10)
     print(f"Average time: {t/10:.6f} seconds\n")
+    times.append(t/10)
+
+sizes = [len(arr) for arr in data]
+plt.plot(sizes, times, 'bo')
+plt.xlabel('Input Size')
+plt.ylabel('Time (Seconds)')
+plt.title('Running Time for "func1"')
+plt.show()
